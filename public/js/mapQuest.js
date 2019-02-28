ee4f462e714bdedec0bf753ade77d491a719627f
Emi Oda <emoda@ucsd.edu>
2019-02-28 10:44:06 -0800

    finished lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'L7nARFzOmK0sMUxXyv9ItfE2JeG6DIcz';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	// add marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
